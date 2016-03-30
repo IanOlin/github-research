9511e25d8c6f08e2c08129f3b845cfb71929ad94
Serena Chen <serenazychen@gmail.com>
2016-03-29 22:34:31 -0400

    added exceptions

--- repo_info.py
+++ repo_info.py
@@ -10,12 +10,15 @@ import os
 import requests
 from mimetools import Message
 from StringIO import StringIO
+import time
 #Generate a list of repos & username associated with that particular repo: list = [(repo1, un1), (repo2, un2), etc.]
 
 repo_collabs = [('Theano', 'Theano'), ('caffe', 'BVLC'), ('CNTK', 'Microsoft'), ('tensorflow', 'tensorflow'), ('torch7', 'torch'), ('deeplearning4j', 'deeplearning4j')]
 keyfile = open('keyfile.txt')
 KEY = keyfile.read()
 keyfile.close()
+
+API_DOMAIN = 'https://api.github.com'
 
 #Use that list's info to get api info/obtain list of collaborators
 
@@ -24,9 +27,11 @@ keyfile.close()
 #grab specific pieces of data & store it
 
 """list of repos with the format (reponame, ownername)"""
-def contributors(repos,dirName):
-	if not os.path.exists(dirName):
-		os.mkdir(dirName)
+def contributors(repos,dirName=None):
+	#print repos
+	if dirName!=None:
+		if not os.path.exists(dirName):
+			os.mkdir(dirName)
 	filenames = []
 	all_repos = {}
 	for collabs in repos:
@@ -35,32 +40,49 @@ def contributors(repos,dirName):
 
 		while page>0:
 
-			URL_str = 'https://api.github.com/repos/{}/{}/contributors?page={}'.format(collabs[1], collabs[0],page)
-			new_URL = requests.get(URL_str)#pattern.web.URL(URL_str).download()
-			print new_URL.headers
+			URL_str = 'https://api.github.com/repos/{}/{}/contributors'.format(collabs[1], collabs[0])
+			new_URL = api_get(base_URL=URL_str, parameters={'page':page, 'access_token':KEY, 'per_page':100})
+
 			index=str(new_URL.headers).find('rel="next"')
 			if index<0:
 				page = -1
 			else:
 				page+=1
 			contributor_data = json.loads(new_URL.text)
-			for contributor in contributor_data:
-				repo_contributors.append(contributor['login'])
+			try:
+				for contributor in contributor_data:
+					# print 'start'
+					# for k in contributor.keys():
+					# 	print '{} {}'.format(k, contributor[k])
+					# print ''
+					repo_contributors.append(contributor['login'])
+			except (KeyError, TypeError) as e:
+				if "Repository access blocked" in contributor_data['message']:
+					continue
+				try:
+					errorfile = open('ERROR.txt', 'w')
+					errorfile.write(json.dumps(contributor_data))
+					errorfile.close()
+				except:
+					print 'could not write to file'
+				raise e
+
 		fname = collabs[0] + 'contributors.txt'
 		filenames.append(fname)
-		f = open(dirName+'/'+fname, 'w')
-		for contributor in repo_contributors:
-			f.write(contributor + '\n')
+		if dirName != None:
+			f = open(dirName+'/'+fname, 'w')
+			for contributor in repo_contributors:
+				f.write(contributor + '\n')
+			f.close()
+
+		all_repos[collabs] = repo_contributors
+
+	if dirName!=None:
+		f = open(dirName+'/'+'files.txt','w')
+		for n in filenames:
+			f.write(n+'\n')
 		f.close()
-		all_repos[collabs[0]] = repo_contributors
-		
-
-	f = open(dirName+'/'+'files.txt','w')
-	for n in filenames:
-		f.write(n+'\n')
-	f.close()
 	
-
 	return all_repos
 
 
@@ -154,9 +176,122 @@ def get_repos(users, dirName=None):
 	
 	return all_repos
 
+def get_contributions(users, dirName = None):
+	"""
+	Take repos
+	if fork, go to repo page
+	go to parent repo
+	if original user is a contributor in parent, add to list of that users contributions
+	"""
+	if dirName!=None:
+		if not os.path.exists(dirName):
+			os.mkdir(dirName)
+	filenames = []
+	all_repos = {}
+
+	for u in users:
+		repos = []
+		page = 1
+
+		while page>0:
+
+			URL_str = '{}/users/{}/repos'.format(API_DOMAIN, u)
+			new_URL = api_get(base_URL=URL_str, parameters={'page':page, 'access_token':KEY, 'per_page':100})
+			# print page
+			index=str(new_URL.headers).find('rel="next"')
+			if index<0:
+				page = -1
+			else:
+				page+=1
+
+			repo_data = json.loads(new_URL.text)
+			try:
+				for repo in repo_data:
+					if repo['fork']:
+						repos.append(repo['name'])
+			except (KeyError,TypeError) as e:
+				try:
+					errorfile = open('ERROR.txt', 'w')
+					errorfile.write(json.dumps(repo_data))
+					errorfile.close()
+				except:
+					print 'could not write to file'
+				raise e
+		fname = u + 'repos.txt'
+		filenames.append(fname)
+		if dirName != None:
+			f = open(dirName+'/'+fname, 'w')
+			for contributor in repo_contributors:
+				f.write(contributor + '\n')
+			f.close()
+
+		
+		contributed = []
+		for fork in repos:
+			"""check for contributor"""
+			repo_URL = "{}/repos/{}/{}".format(API_DOMAIN, u,fork)
+			page = json.loads(api_get(base_URL=repo_URL, parameters={'access_token':KEY}).text)
+			try:
+				parent_repo = (page['parent']['name'], page['parent']['owner']['login'])
+			except (KeyError, TypeError) as e:
+				if "Repository access blocked" in page['message']:
+					continue
+				try:
+					errorfile = open('ERROR.txt', 'w')
+					errorfile.write(json.dumps(page))
+					errorfile.close()
+				except:
+					print 'could not write to file'
+				raise e
+
+			people = contributors([parent_repo])
+			#print people
+			if u in people[parent_repo]:
+				contributed.append(parent_repo)
+
+		all_repos[u] = contributed
+		
+	if dirName!=None:
+		f = open(dirName+'/'+'files.txt','w')
+		for n in filenames:
+			f.write(n+'\n')
+		f.close()
+	
+	return all_repos
+
+def api_get(base_URL, parameters = {}, min_remaining=1):
+	"""If there are not enough calls remaining, wait until they refresh"""
+	while True:
+		try:
+			URL_str = "https://api.github.com/rate_limit"
+			response = requests.get(URL_str, params={'access_token':KEY})
+			try:
+				remaining = int(json.loads(response.text)['resources']['core']['remaining'])
+			except KeyError as e:
+				try:
+					errorfile = open('ERROR.txt', 'w')
+					errorfile.write(response.text)
+					errorfile.close()
+				except:
+					print 'could not write to file'
+				raise e
+
+			if remaining < min_remaining:
+				print 'waiting'
+				time_sec = int(json.loads(response.text)['resources']['core']['reset'])
+				time.sleep(time_sec+5-int(time.time()))
+				print 'done waiting'
+			return requests.get(base_URL, params=parameters)
+		except requests.exceptions.ConnectionError:
+			print 'ConnectionError, trying again'
+			continue
+		break
+
 
 if __name__=='__main__':
+	print get_contributions(['Yangqing'])
 	pass
+
 #h=contributors(repo_collabs,'mlcontrib')
 	# collaborators(repo_collabs,'mlcollabs')
 	# forks(repo_collabs,'mlforks')
--- reposOfContributors.py
+++ reposOfContributors.py
@@ -3,77 +3,84 @@ import json
 
 dir = 'mlcontrib/'
 names = open(dir+'files.txt','r').read()
-#filelist = names.split()
-filelist = ['Theanocontributors.txt']
+filelist = names.split()
+
 print filelist
+
+repo_collabs = {'Theano': 'Theano','caffe': 'BVLC','CNTK': 'Microsoft','tensorflow': 'tensorflow', 'torch7': 'torch', 'deeplearning4j': 'deeplearning4j'}
+
+# for n in filelist:
+#     x = n.split('contributors')[0]
+#     print x
+#     print repo_collabs[x]
 
 #dictionary: 
 #base repo name : list of forked repos
-#maybe??
+#maybe?? 
 
 
 #all collaborators mapped name:index
 #collaborator name : index in matrix
-# allCollaborators = {}
+allCollaborators = {}
 #names of all projects, indexes corresponding with the index in matrix
 #project name : index in matrix
-# projects = {}
+projects = {}
 #matrix of projects (rows) and contributors (columns)
-# matrix = []
-# for i in range(len(filelist)):
-# 	matrix.append([0]*len(allCollaborators))
-#index of the next new repo introduced via contributor
-# extraIndexes = len(filelist)
-
+matrix = []
 for i in range(len(filelist)):
-    allContribRepos = {}
-    projectName = filelist[i].split('contributors')[0]
-    # projects[projectName]=i
-    data1 = open(dir+filelist[i], 'r').read()
-    data1 = data1.split()
-    for c in data1:
-        # if c not in allCollaborators:
-        #     allCollaborators[c] = addCol(matrix)#next index
-        # else:
-        #     print allCollaborators[c]
-        # matrix[i][allCollaborators[c]] = 1
-        individualRepos = repo_info.get_repos([c])[c]
-        #individualReposForked = individualRepos[1]
-        # for fork in individualReposForked:
-        # 	index = extraIndexes
-        # 	if fork in projects.keys():
-        # 		index = project[fork]
-        # 	else:
-        # 		extraIndexes+=1
-        # 		projects[fork]=index
-        # 		matrix.append([0]*len(allCollaborators[0]))
-        	# matrix[projects[fork]][allCollaborators[c]] = 1
-        allContribRepos[c] = individualRepos
-    rawfile = open(projectName+'ContribRepos', 'w')
-    json.dump(obj=allContribRepos,fp=rawfile)
-    rawfile.close()
-# f=open('extendedcontributors.txt','w')
-# f.write("dl nr={} nc={} format=fullmatrix\n".format(len(projects),len(allCollaborators)))
-# f.write("row labels:\n")
-# for p in projects:
-#     f.write(p+' ')
-# f.write('\n')
-# f.write('column labels:\n')
-# for x in sorted(allCollaborators.items(),key=lambda x:x[1]):
-#     f.write(x[0]+' ')
-# f.write('\n')
-# for row in matrix:
-#     for i in row:
-#         f.write(str(i)+' ')
-#     f.write('\n')
-# f.close()
-
-
-
+	matrix.append([0]*len(allCollaborators))
+#index of the next new repo introduced via contributor
+extraIndexes = len(filelist)
 
 def addCol(adjMatrix):
     """Adds a column to the end of adjMatrix and returns the index of the comlumn that was added"""
     for j in range(len(adjMatrix)):
         adjMatrix[j].append(0)
     return len(adjMatrix[0])-1
+
+
+for i in range(len(filelist)):
+    print 'repo'
+    allContribRepos = {}
+    projectName = filelist[i].split('contributors')[0]
+    projects[(projectName, repo_collabs[projectName])]=i
+    data1 = open(dir+filelist[i], 'r').read()
+    data1 = data1.split()
+    for c in data1:
+        if c not in allCollaborators:
+            allCollaborators[c] = addCol(matrix)#next index
+        matrix[i][allCollaborators[c]] = 1
+        individualRepos = repo_info.get_contributions([c])[c]
+        for fork in individualRepos:
+        	index = extraIndexes
+        	if fork in projects.keys():
+        		index = projects[fork]
+        	else:
+        		extraIndexes+=1
+        		projects[fork]=index
+        		matrix.append([0]*len(allCollaborators.keys()))
+        	matrix[projects[fork]][allCollaborators[c]] = 1
+        allContribRepos[c] = individualRepos
+    rawfile = open(projectName+'ContribRepos', 'w')
+    json.dump(obj=allContribRepos,fp=rawfile)
+    rawfile.close()
+f=open('extendedcontributorsnew.txt','w')
+f.write("dl nr={} nc={} format=fullmatrix\n".format(len(projects),len(allCollaborators)))
+f.write("row labels:\n")
+for p in projects:
+    f.write(p+' ')
+f.write('\n')
+f.write('column labels:\n')
+for x in sorted(allCollaborators.items(),key=lambda x:x[1]):
+    f.write(x[0]+' ')
+f.write('\n')
+for row in matrix:
+    for i in row:
+        f.write(str(i)+' ')
+    f.write('\n')
+f.close()
+
+
+
+
 
