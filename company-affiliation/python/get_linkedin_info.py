b888a6d638675415aec803788c1ce1ef305af327
kuannie1 <anne.ku4@gmail.com>
2016-10-26 01:24:34 -0400

    Current progress with LinkedIn Data, includes some scraped data and updated script

--- company-affiliation/python/get_linkedin_info.py
+++ company-affiliation/python/get_linkedin_info.py
@@ -11,29 +11,47 @@ from pattern.web import URL, extension, download
 import json
 from selenium import webdriver
 from pyvirtualdisplay import Display
-
+from sets import Set
 import os
 import json
 
 PATH = "../resources/linkedin_info/"
 DEBUG = True
 
-def findlinkedininfo(name_list): #name is a tuple, predefinedlinkedin is a boolean
+# shas = []
+# names = []
+# dates = []
+# companies = []
+
+problemPeople = []
+indexToStart = 0
+START_AT_INDEX = False
+def findlinkedininfo(name_list): #name is a tuple, predefinedlinkedin is a boolean if we can implement it
 	"""
-	Takes in a list of names as a tuple, and a boolean
+	Takes in a list of names as a tuple and returns a file for each name if there is linkedin info for that name
 	"""
 	allcompanies = []
-	for name in name_list:
+
+	if START_AT_INDEX:
+		with open("index.txt", 'r') as f:
+			s = f.read(indexToStart).strip();
+			global indexToStart
+			indexToStart = int(s)
+
+	for j in range(indexToStart, len(name_list)):
+		name = name_list[j]
+		print name
 		driver = webdriver.Firefox(capabilities=firefox_capabilities)
 		if DEBUG:
 			print driver
 		#What to put in the open() URL area for Geetika Batra: https://www.google.com/#q=Geetika+Batra+linkedin
-		driver.get('https://www.google.com/#q={}+{}+linkedin'.format(name[0], name[1]))
+		driver.get('https://www.google.com/#q={}+linkedin'.format(name))
 		time.sleep(2)
 		google_results = driver.find_elements_by_class_name("r") # should get the headlines/main title of each google result
+
 		orgsAndCompanies = []
 		for res in google_results:
-			if (str(res.text.encode('ascii', 'ignore').decode('ascii'))) == "{} {} | LinkedIn".format(name[0], name[1]):
+			if (str(res.text.encode('ascii', 'ignore').decode('ascii'))) == "{} | LinkedIn".format(name):
 				#getting the index that fits the if statement's qualifications
 				index = google_results.index(res)
 				if DEBUG:
@@ -44,26 +62,38 @@ def findlinkedininfo(name_list): #name is a tuple, predefinedlinkedin is a boole
 				# opening the Linkedin URL
 				driver.get(profile_url)
 				time.sleep(2)
+				title = driver.find_element_by_tag_name('title').text.encode('ascii', 'ignore').decode('ascii')
+				if (title == "Sign Up | LinkedIn"):
+					indexToStart = j
+					print j
+					driver.quit()
+					print problemPeople
+					return allcompanies
 				orgs_worklife = driver.find_elements_by_class_name("item-subtitle")
 				dateranges = driver.find_elements_by_class_name("date-range")
 
 
-				for daterange in dateranges:
-					one_daterange = daterange.text.encode('ascii', 'ignore').decode('ascii')
-					org_index = dateranges.index(daterange)
-					one_org = orgs_worklife[org_index].text.encode('ascii', 'ignore').decode('ascii')
-					orgsAndCompanies.append((one_org, one_daterange))
+				for i in range(len(dateranges)):
+					try:
+						daterange = dateranges[i]
+						one_daterange = daterange.text.encode('ascii', 'ignore').decode('ascii')
+						print i
+						one_org = orgs_worklife[i].text.encode('ascii', 'ignore').decode('ascii')
+						orgsAndCompanies.append((one_org, one_daterange))
+					except IndexError as e:
+						problemPeople.append("{}, {}, {}".format(name, i, orgsAndCompanies))
+						break
 
 				driver.close()
 				time.sleep(2)
 				break
-		f = open(PATH+'{}_{}_linkedin.json'.format(name[0], name[1]), 'w')
+		f = open(PATH+'{}_linkedin.json'.format(name), 'w')
 		json.dump(orgsAndCompanies, f)
 		allcompanies.append(orgsAndCompanies)
 		f.close()
 		driver.quit()
+	print problemPeople
 	return allcompanies
-
 
 """
 Changing the directory so that the headline files are stored in a convenient place
@@ -83,9 +113,13 @@ def changeDirectory():
 	if DEBUG:
 		print "Directory changed successfully %s" % retval
 
-def commitsToCompanies(filename):
-	#TODO: actually make this method
 
+"""
+Takes in all the results of the json file and returns the info we really need in a tuple
+returns: (date list, sha list, name list)
+Keep in mind the names aren't in their final, simplified form. There are duplicates in this names list.
+"""
+def obtainDatesShasNames(filename):
 	commits = json.load(open(filename, 'r'))
 	shas = []
 	names = []
@@ -94,10 +128,39 @@ def commitsToCompanies(filename):
 	for c in commits: 
 		#TODO:
 		#store dates in dates
-		dates.append(c["commit"]["author"]["date"])
+		dates.append(c["commit"]["author"]["date"].encode("utf-8"))
 		#store sha in shas
+		commiturl = c["commit"]["url"]
+		commiturl_list = commiturl.split("/")
+		shas.append(commiturl_list[-1].encode("utf-8"))
 		#store name in names
+		names.append(c["commit"]["author"]["name"].encode("utf-8"))
 		pass
+	filterLists(dates, shas, names)
+	return (dates, shas, names)
+
+"""
+Gets one of each name and stores the results into a list
+"""
+def simplifyNameList(comprehensiveNameList):
+	newNameSet = Set()
+	for name in comprehensiveNameList:
+		newNameSet.add(name)
+	return newNameSet
+
+""" 
+eliminate Jenkins, OpenStack Proposal Bot, and Openstack Jenkins
+Also replaces the email with the person's actual name
+"""
+def filterLists(dates, shas, names):
+	for i in range(len(names) - 1, -1, -1):
+		n = names[i]
+		if (n == 'OpenStack Proposal Bot') or (n == 'Jenkins') or (n == 'OpenStack Jenkins'):
+			index_location = names.index(n)
+			del names[index_location]
+			del shas[index_location]
+			del dates[index_location]
+
 	#TODO:
 	#call findlinkedininfo on list of names, save in companies
 	#change all Nones to "no company" with a flag of 0, change all other companies to a flag of like 3.
@@ -105,9 +168,17 @@ def commitsToCompanies(filename):
 	#zip shas and companies to a dict or something
 	#save to file
 
-"""
-returns the element or json object from the tuple of keys
-"""
+
+def commitsToCompanies(filename):
+	"""
+	returns the element or json object from the tuple of keys
+	"""
+	(dates, shas, names) = obtainDatesShasNames(filename)
+	#Getting one of each name
+	finalnamelist = list(simplifyNameList(names))
+	findlinkedininfo(finalnamelist)
+
+
 def JSON_access(jsonObject, keyTuple):
     try:
         breakdown = jsonObject
@@ -122,13 +193,17 @@ def JSON_access(jsonObject, keyTuple):
 
 if __name__ == '__main__':
 	pathtojsons = "/home/anne/"
-	companyfile = [pathtojsons+"glance-openstack-commits.json"] # + "filename"
+	companyfile = pathtojsons+"glance-openstack-commits.json" # + "filename"
 	print "hello!"
 	# names = [("Christina", "Tipps") , ("Chmouel", "Boudjnah"), ("Geetika", "Batra")]
 	# name_history = [] 
 	# print findlinkedininfo(names)
-	commitsToCompanies(companyfile[0])
-
+	# (dates, shas, names) = obtainDatesShasNames(companyfile)
+	commitsToCompanies(companyfile)
+	with open("index.txt", 'w') as f:
+		f.write(str(indexToStart))
+	with open("problempeople.txt", 'w') as f:
+		f.write(str(problemPeople))
 
 
 	# # Block of code to get the contents from the linkedin profile
