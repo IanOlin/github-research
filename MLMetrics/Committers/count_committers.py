571e35999352eeb2bd83b579ca80916812ebaa0e
Serena Chen <serenazychen@gmail.com>
2017-03-10 14:41:45 -0500

    file to count committers + commits"

---
+++ MLMetrics/Committers/count_committers.py
@@ -0,0 +1,34 @@
+import json
+
+pathToJSON = "/home/serena/GithubResearch/mlCommits-new/"
+# pathToJSON = "/home/anne/ResearchJSONs/"
+fileList = ("caffe-BVLC-commits.json", "CNTK-Microsoft-commits.json", "deeplearning4j-deeplearning4j-commits.json", "tensorflow-tensorflow-commits.json", "Theano-Theano-commits.json", "torch7-torch-commits.json", "incubator-systemml-apache-commits.json")
+
+TENSORFLOWER_GARDENER_NAME = "A. Unique TensorFlower"
+
+"""
+Returns a dict of {committer name : number of commits}
+"""
+def count_commits_per_user(filename):
+    raw = openJSON(pathToJSON + filename)
+    userCommitHist = {}
+    for commit in raw:
+        # simple fields
+        name = commit["commit"]["author"]["name"].encode("utf-8")
+        if(name != TENSORFLOWER_GARDENER_NAME):
+            userCommitHist[name] = userCommitHist.get(name, 0) + 1
+    return userCommitHist
+
+def getRepoID(fname):
+    filePieces = fname.split("-")
+    repo = filePieces[0]+"-"+filePieces[1]
+    return repo
+
+def openJSON(fname):
+    return json.load(open(fname, "r"))
+
+if __name__ == "__main__":
+    for fn in fileList:
+        print fn
+        commit_dict = count_commits_per_user(fn)
+        print len(commit_dict.keys())
