982cd4a9721d17442f1114c038d667666bf3f8c1
Serena Chen <serenazychen@gmail.com>
2017-04-04 16:45:04 -0400

    made a file of general constants (feel free to add to them), updated pull request metric code to reflect

--- MLMetrics/PullRequests/pr_metrics.py
+++ MLMetrics/PullRequests/pr_metrics.py
@@ -2,59 +2,74 @@ import json
 import sys, os
 sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
 from github_scraping.get_pull_requests import is_merged_systemml
+from constants import ML, STACK, return_constants
 
 directory = '../../github_scraping/mlpulls/'
-# /home/serena/GithubResearch/github-research/github-scraping/mlpulls/CNTK_pulls.json
 
-# hard copied from repo_info
-mlRepos = {'Theano' : 'Theano',
-    'caffe' : 'BVLC',
-    'CNTK' : 'Microsoft',
-    'tensorflow' : 'tensorflow',
-    'torch7' : 'torch',
-    'deeplearning4j': 'deeplearning4j',
-    'incubator-systemml' : 'apache'}
 systemml_name = 'incubator-systemml'
 
-"""
-Metrics overall
-"""
-for repo, owner in mlRepos.items():
-    print repo, owner
-    pulls = json.load(open(directory + "{}_pulls.json".format(repo)))
-    print 'num_pulls,{}'.format(len(pulls))
-    merged = 0
-    closed = 0
-    open_pulls = 0
-    merged_users = {}
-    closed_users = {}
-    for p in pulls:
-        merged_at = p["merged_at"]
-        closed_at = p["closed_at"]
-        username = p["user"]["login"]
-        if closed_at != None and repo == systemml_name:
-            merged_at = 1 if is_merged_systemml(p) else None
+def sort_by_val_return_str(d):
+    """
+    Takes a dict, sorts it by the value, and returns the string with key:value
+    in reverse order by value
+    """
+    string = ""
+    for u in sorted(d.items(), key=lambda x:x[1], reverse=True):
+        string += " {}:{}".format(u[0], u[1])
+    return string
 
-        if merged_at != None:
-            merged += 1
-            merged_users[username] = merged_users.get(username, 0) + 1
-        elif closed_at != None:
-            closed += 1
-            closed_users[username] = closed_users.get(username, 0) + 1
-        else:
-            open_pulls += 1
+def print_prs(project_flag):
+    """
+    Calculates the number of merged and closed pull requests, as well as the high
+    frequency pull requesters, and prints to stdout. Takes in a flag, defined by
+    constants.py, that determines whether to look at stack repos or ml repos.
+    """
+    repos = return_constants(project_flag)["repos"]
+    for repo_dict in repos:
+        repo = repo_dict["name"]
+        owner = repo_dict["user"]
+        print repo, owner
 
-    merged_users_str = ""
-    for u in sorted(merged_users.items(), key=lambda x:x[1], reverse=True):
-        merged_users_str += " {}:{}".format(u[0], u[1])
+        # grab pull requests
+        pulls = json.load(open(directory + "{}_pulls.json".format(repo)))
+        print 'num_pulls,{}'.format(len(pulls))
 
-    closed_users_str = ""
-    for u in sorted(closed_users.items(), key=lambda x:x[1], reverse=True):
-        closed_users_str += " {}:{}".format(u[0], u[1])
+        # sort
+        merged = 0
+        closed = 0
+        open_pulls = 0
+        merged_users = {}
+        closed_users = {}
+        for p in pulls:
+            merged_at = p["merged_at"]
+            closed_at = p["closed_at"]
+            username = p["user"]["login"]
+            # make an exception for systemml
+            if closed_at != None and repo == systemml_name:
+                merged_at = 1 if is_merged_systemml(p) else None
 
-    print 'num_merged,{}'.format(merged)
-    print 'merged_users,{}'.format(merged_users_str)
-    print 'num_open,{}'.format(open_pulls)
-    print 'num_closed,{}'.format(closed)
-    print 'closed_users,{}'.format(closed_users_str)
-    print ''
+            # count
+            if merged_at != None:
+                merged += 1
+                merged_users[username] = merged_users.get(username, 0) + 1
+            elif closed_at != None:
+                closed += 1
+                closed_users[username] = closed_users.get(username, 0) + 1
+            else:
+                open_pulls += 1
+
+        # sort all users who have had merged prs
+        merged_users_str = sort_by_val_return_str(merged_users)
+
+        # sort all users that have closed prs (unmerged)
+        closed_users_str = sort_by_val_return_str(closed_users)
+
+        # statistics for that repo
+        print 'num_merged,{}'.format(merged)
+        print 'merged_users,{}'.format(merged_users_str)
+        print 'num_open,{}'.format(open_pulls)
+        print 'num_closed,{}'.format(closed)
+        print 'closed_users,{}'.format(closed_users_str)
+        print ''
+
+print_prs(ML)
---
+++ constants.py
@@ -0,0 +1,46 @@
+# flags
+ML = 0
+STACK = 1
+
+# Dictionaries of constants; use the `return_constants()` function
+# to access
+__ML_CONSTANTS = {}
+__ST_CONSTANTS = {}
+
+# Adding repo names and repo users
+ml_repos = []
+ml_repos.append({"name": 'Theano',              "user": 'Theano'})
+ml_repos.append({"name": 'caffe',               "user": 'BVLC'})
+ml_repos.append({"name": 'CNTK',                "user": 'Microsoft'})
+ml_repos.append({"name": 'tensorflow',          "user": 'tensorflow'})
+ml_repos.append({"name": 'torch7',              "user": 'torch'})
+ml_repos.append({"name": 'deeplearning4j',      "user": 'deeplearning4j'})
+ml_repos.append({"name": 'incubator-systemml',  "user": 'apache'})
+
+__ML_CONSTANTS["repos"] = ml_repos
+
+stack_repos = []
+stack_repos.append({"name": 'cinder',           "user": 'openstack'})
+stack_repos.append({"name": 'glance',           "user": 'openstack'})
+stack_repos.append({"name": 'horizon',          "user": 'openstack'})
+stack_repos.append({"name": 'keystone',         "user": 'openstack'})
+stack_repos.append({"name": 'nova',             "user": 'openstack'})
+stack_repos.append({"name": 'neutron',          "user": 'openstack'})
+stack_repos.append({"name": 'swift',            "user": 'openstack'})
+stack_repos.append({"name": 'cloudstack',       "user": 'apache'})
+
+__ST_CONSTANTS["repos"] = ml_repos
+
+# Jenkins names
+__ML_CONSTANTS["jenkins"] = ("A. Unique TensorFlower")
+__ST_CONSTANTS["jenkins"] = ("Jenkins", 'OpenStack Proposal Bot')
+
+def return_constants(flag):
+    """
+    Given a flag, returns the correct dictionary of constants
+    """
+    if flag == ML:
+        return __ML_CONSTANTS
+    if flag == STACK:
+        return __ST_CONSTANTS
+    raise Exception("unknown flag")
