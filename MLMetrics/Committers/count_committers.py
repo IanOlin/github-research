047af64690d74f22865083112b7004be9591a360
kuannie1 <anne.ku4@gmail.com>
2017-04-06 23:26:26 -0400

    update count_committers

--- MLMetrics/Committers/count_committers.py
+++ MLMetrics/Committers/count_committers.py
@@ -2,13 +2,9 @@ import json
 import csv
 import os, sys
 sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
-from misc_info.constants import ML, STACK, CURRENT_DATE, return_constants
+from misc_info.constants import ML, STACK, CURRENT_DATE, return_constants, return_filename
 
-pathToJSON = "/home/jwb/Documents/Json/"
-# pathToJSON = "/home/anne/ResearchJSONs/"
-fileList = ("caffe-BVLC-commits.json", "CNTK-Microsoft-commits.json", "deeplearning4j-deeplearning4j-commits.json", "tensorflow-tensorflow-commits.json", "Theano-Theano-commits.json", "torch7-torch-commits.json", "incubator-systemml-apache-commits.json")
-
-TENSORFLOWER_GARDENER_NAME = "A. Unique TensorFlower"
+# fileList = ("caffe-BVLC-commits.json", "CNTK-Microsoft-commits.json", "deeplearning4j-deeplearning4j-commits.json", "tensorflow-tensorflow-commits.json", "Theano-Theano-commits.json", "torch7-torch-commits.json", "incubator-systemml-apache-commits.json")
 
 constants_dict = {}
 
@@ -16,30 +12,28 @@ constants_dict = {}
 Returns a dict of {committer name : number of commits}
 """
 def count_commits_per_user(filename):
-    raw = openJSON(pathToJSON + filename)
+    global constants_dict
+    raw = openJSON(constants_dict["commits-fpath"] + filename)
     userCommitHist = {}
     for commit in raw:
         # simple fields
         name = commit["commit"]["author"]["name"].encode("utf-8")
-        if(name != TENSORFLOWER_GARDENER_NAME):
+        if(name not in constants_dict["jenkins"]):
             userCommitHist[name] = userCommitHist.get(name, 0) + 1
     return userCommitHist
-
-def getRepoID(fname):
-    filePieces = fname.split("-")
-    repo = filePieces[0]+"-"+filePieces[1]
-    return repo
 
 def openJSON(fname):
     return json.load(open(fname, "r"))
 
 if __name__ == "__main__":
     constants_dict = return_constants(ML)
-    for fn in fileList:
+    for repo in constants_dict["repos"]:
         #print fn
-        commit_dict = count_commits_per_user(fn)
+        # obtain a dictionary of commiters: # commits :
+        commit_dict = count_commits_per_user(return_filename(repo))
         #print len(commit_dict.keys())
-        with open(pathToJSON + "csvs/"+fn[:-5]+'-dict.csv', 'w') as csv_file:
+        output_file = "csvs_new/{}-{}-dict.csv".format(repo["name"], repo["user"])
+        with open(output_file, 'w') as csv_file:
             writer = csv.writer(csv_file)
             for k, v in commit_dict.items():
-                writer.writerow([k.decode('utf8'), v])
+                writer.writerow([k, v])
